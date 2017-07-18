577cdc7d06907cc7abdfca02601e9fe601a58219
echosteg <email@echosteg.com>
2017-07-18 22:35:52 +0530

    documentation

--- src/components/HelloWorld/HelloWorld.js
+++ src/components/HelloWorld/HelloWorld.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
+/** A component to demonstrate document generation */
 const HelloWorld = ({message}) => <div>Hello {message}</div>;
 
 HelloWorld.propTypes = {
+    /** message to display */
     message: PropTypes.string
 };
 
+HelloWorld.defaultProps = {
+    message: 'World'
+}
 export default HelloWorld;
---
+++ src/docs/examples/HelloWorld/ExampleHelloWorld.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import HelloWorld from 'react-cynx-blob/HelloWorld';
+
+/** Custom message */
+export default function ExampleHelloWorld() {
+    return <HelloWorld message="Pluralsight viewers!" />
+}
